6a02f42541c1b6fa7eb03350b35e00241dba191d
hibernonnahu <52815511+hibernonnahu@users.noreply.github.com>
2023-03-05 16:29:50 -0300

    Main

---
+++ Main/Scripts/StateMachine/Game/StateGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class StateGame : State<StateGame>
+{
+    public StateGame(StateMachine<StateGame> stateMachine) :base (stateMachine)
+    {
+       
+    }
+
+
+    public virtual void Spawn()
+    {
+       
+    }
+
+    public virtual void OnDestroy()
+    {
+        
+    }
+}
