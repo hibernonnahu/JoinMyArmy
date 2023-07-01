59ef22ceb804753d6a257ac1521257546cfc5ec5
Nahu UY <hibernon.nm@gmail.com>
2023-07-01 00:03:54 -0300

    resources

---
+++ Assets/Resources/Scripts/ChestSkills/SkillExtraSpeed.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillExtraSpeed : ISkill
+{
+    public void ExecuteAura()
+    {
+    }
+
+    public void ExecuteOnGrab(Character character)
+    {
+
+    }
+
+    public void ExecuteOnHit(Character characterHit, float damage)
+    {
+
+    }
+
+    public void ExecuteOnKill(Character character, Character characterHit)
+    {
+
+    }
+    public int ExtraRecluit()
+    {
+        return 0;
+    }
+    public int ExtraDamage()
+    {
+        return 0;
+    }
+    public float ExtraSpeed()
+    {
+        return 0.3f;
+    }
+    public int ExtraDefense()
+    {
+        return 0;
+    }
+
+    public int ExtraHealth()
+    {
+        return 0;
+    }
+
+    public string GetDescription()
+    {
+        return "Boost speed";
+    }
+
+    public string GetName()
+    {
+        return "Attack Speed";
+    }
+
+    public bool IsAvailable()
+    {
+        return true;
+    }
+
+
+}
